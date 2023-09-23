68463e2542532dfd6c7336af870c213415001d80
EliasDeHondt <Elias.dehondt@outlook.com>
2023-09-23 21:36:35 +0200

    Minor changes to logic and .gitignore file.

--- CA/ConsoleUi.cs
+++ CA/ConsoleUi.cs
@@ -22,7 +22,7 @@ public class ConsoleUi
         {
             Console.WriteLine("What would you like to do?");
             Console.WriteLine("===============================");
-            Console.WriteLine("0) Quit\n1) Show all Players\n2) Show players by position\n3) Show all Padel Courts\n4) Show Padel Courts with CourtNumber and/or (Indoor?)");
+            Console.WriteLine("0) Quit\n1) Show all Players\n2) Show players by position\n3) Show all Padel Courts\n4) Show Padel Courts with Price and/or (Indoor?)");
             Console.Write("Choice (0-4): ");
             string Input = Console.ReadLine();
             
@@ -65,22 +65,72 @@ public class ConsoleUi
                     }
                     break;
                 case "4":
-                    Console.WriteLine("Which court number would you like to see?");
-                    string InputCourtNumber = Console.ReadLine();
+                    Double Price = 0.0; // Set the price to 0.0 by default
+                    Boolean NewInputIndoor = false; // Set the indoor/outdoor to false by default
+                    Boolean InputFirstQuestion = true;
+                    Boolean IsParsedPrice;
+                    Boolean InputSecondQuestion = true;
+                    string InputPrice = "";
+                    string InputIndoor = "";
+                    
+                    while (InputFirstQuestion)
+                    {
+                        Console.Write("Enter te price of the Padel Court or leave blank: ");
+                        InputPrice = Console.ReadLine();
+                        
+                        IsParsedPrice = Double.TryParse(InputPrice, out Price); // Try to parse the input to a double
+                        // If the input is a double, set InputFirstQuestion to false (stop the while loop)
+                        // of
+                        // If the input is null, empty or whitespace, set InputFirstQuestion to false (stop the while loop)
+                        if (IsParsedPrice || string.IsNullOrWhiteSpace(InputPrice)) InputFirstQuestion = false;
+                    }
+
+                    while (InputSecondQuestion)
+                    {
+                        Console.Write("Enter (I)ndoor or (O)utdoor or leave blank: ");
+                        InputIndoor = Console.ReadLine();
+                        InputIndoor = InputIndoor.ToLower();
+                        if (InputIndoor == "i" || InputIndoor == "o") InputSecondQuestion = false; // If the input is "i" or "o", set InputSecondQuestion to false (stop the while loop)
+                        if (string.IsNullOrWhiteSpace(InputIndoor)) InputSecondQuestion = false; // If the input is null, empty or whitespace, set InputSecondQuestion to false (stop the while loop)
+                        if (InputIndoor == "i") NewInputIndoor = true; else NewInputIndoor = false; // If the input is "i", set NewInputIndoor to true, else set it to false
+                    }
                     
                     
-                    
-                    Console.WriteLine("Would you like to see indoor courts? (y/n)");
-                    string InputIndoor = Console.ReadLine();
-                    
-                    
+                    foreach (PadelCourt padelCourt in PadelCourts)
+                    {
+                        if (string.IsNullOrWhiteSpace(InputPrice) == false && string.IsNullOrWhiteSpace(InputIndoor) == false) // If both inputs are not null, empty or whitespace
+                        {
+                            if (padelCourt.Price == Price && padelCourt.IsIndoor == NewInputIndoor) // If the price and indoor/outdoor match
+                            {
+                                if (Price != 0.0) Console.WriteLine(padelCourt.ToString()); // If the price is not 0.0, show the Padel Court
+                            }
+                        }
+                        else if (string.IsNullOrWhiteSpace(InputPrice) == false) // If only the price input is not null, empty or whitespace
+                        {
+                            if (padelCourt.Price == Price) // If the price matches
+                            {
+                                if (Price != 0.0) Console.WriteLine(padelCourt.ToString()); // If the price is not 0.0, show the Padel Court
+                            }
+                        }
+                        else if (string.IsNullOrWhiteSpace(InputIndoor) == false) // If only the indoor/outdoor input is not null, empty or whitespace
+                        {
+                            if (padelCourt.IsIndoor == NewInputIndoor) // If the indoor/outdoor matches
+                            {
+                                Console.WriteLine(padelCourt.ToString());
+                            }
+                        }
+                        else // If both inputs are null, empty or whitespace
+                        {
+                            Console.WriteLine(padelCourt.ToString());
+                        }
+                    }
                     break;
             }
         }
 
     }
 
-    public void Seed()
+    public void Seed() // Seed the application with some data
     { 
         // Seed data for Club (1 Object)
         Club Club = new Club(); // Create a new instance of Club
@@ -90,7 +140,7 @@ public class ConsoleUi
         Club.HouseNumber = 3; // Set the house number
         Club.ZipCode = 2650; // Set the zip code
         
-        // Seed data for PadelCourts (5 Objects)
+        // Seed data for PadelCourts (6 Objects)
         PadelCourts.Add(new PadelCourt
         {
             CourtNumber = 1, 
@@ -126,9 +176,17 @@ public class ConsoleUi
         PadelCourts.Add(new PadelCourt
         {
             CourtNumber = 5, 
-            IsIndoor = false, 
+            IsIndoor = false,
             Capacity = 2, 
             Price = 15.75, 
+            Club = Club // (1 op veel relatie)
+        });
+        PadelCourts.Add(new PadelCourt
+        {
+            CourtNumber = 6, 
+            IsIndoor = false, 
+            Capacity = 2, 
+            Price = 20.50, 
             Club = Club // (1 op veel relatie)
         });
         
@@ -140,7 +198,7 @@ public class ConsoleUi
             BirthDate = new DateOnly(2001, 4, 10), 
             Level = 5.5, 
             Position = PlayerPosition.Member, 
-            PlayedOnCourts = new List<PadelCourt> {PadelCourts[0], PadelCourts[1]}  // 2 courts (veel-op-veel relatie)
+            PlayedOnCourts = new List<PadelCourt> {PadelCourts[0], PadelCourts[1], PadelCourts[5]}  // 3 courts (veel-op-veel relatie)
         });
         Players.Add(new Player
         {
