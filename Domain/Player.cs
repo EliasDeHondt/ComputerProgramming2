31229cd67ae918833c4487fadc6bec7ebaa95daa
EliasDeHondt <Elias.dehondt@outlook.com>
2023-10-19 16:48:10 +0200

    RX update voor DateOnly? -> Player

--- Domain/Player.cs
+++ Domain/Player.cs
@@ -19,7 +19,7 @@ public class Player
     public string FirstName { get; set; }
     [StringLength(50, MinimumLength = 2, ErrorMessage = "(LastName) At least 2 character, maximum 50 characters")] [Required]
     public string LastName { get; set; }
-    [RegularExpression(@"\d{2}/\d{2}/\d{4}", ErrorMessage = "(BirthDate) Input the date as follows: dd/MM/yyyy")]
+    [RegularExpression(@"\d{1,2}/\d{1,2}/\d{4}", ErrorMessage = "(BirthDate) Input the date as follows: dd/MM/yyyy")]
     public DateOnly? BirthDate { get; set; }
     [Range(0, 10, ErrorMessage = "(Level) Input a number from 0 to 10")] [Required]
     public double Level { get; set; }
