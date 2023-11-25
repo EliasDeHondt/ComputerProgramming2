d046c3226ece80bcae4ce2a8d762d94736331e39
EliasDeHondt <Elias.dehondt@outlook.com>
2023-11-25 17:52:19 +0100

    Part 2 of Sprint 5

---
+++ UI-MVC/Controllers/PadelCourtController.cs
@@ -0,0 +1,30 @@
+/***************************************
+ *                                     *
+ *   Created by Elias De Hondt         *
+ *   Visit https://eliasdh.com         *
+ *                                     *
+ ***************************************/
+// Controller PadelCourt
+
+using Microsoft.AspNetCore.Mvc;
+using PadelClubManagement.BL;
+using PadelClubManagement.BL.Domain;
+
+namespace PadelClubManagement.UI.Web.Controllers;
+
+public class PadelCourtController : Controller
+{
+    private readonly IManager _manager;
+
+    public PadelCourtController(IManager manager)
+    {
+        _manager = manager;
+    }
+
+    public IActionResult Detail(int courtNumber)
+    {
+        PadelCourt padelCourt = _manager.GetPadelCourt(courtNumber);
+        if (padelCourt == null) return NotFound();
+        return View(padelCourt);
+    }
+}
