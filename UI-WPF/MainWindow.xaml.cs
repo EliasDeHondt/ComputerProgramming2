a349a14dc806f59bfebbb9b167332f4c37eba1d1
EliasDeHondt <Elias.dehondt@outlook.com>
2023-12-14 14:50:39 +0100

    Dit is EXTRA!!! Dit is een Desktop app v5

--- UI-WPF/MainWindow.xaml.cs
+++ UI-WPF/MainWindow.xaml.cs
@@ -14,56 +14,55 @@ using PadelClubManagement.BL;
 using PadelClubManagement.BL.Domain;
 using PadelClubManagement.DAL.EF;
 
-namespace PadelClubManagement.UI.WIN
-{
-    public partial class MainWindow : Window
+namespace PadelClubManagement.UI.WIN;
+
+public partial class MainWindow : Window
+{ 
+    private IManager _manager;
+    private PadelClubManagementDbContext _context;
+
+    public MainWindow()
     {
-        private IManager _manager;
-        private PadelClubManagementDbContext _context;
+        InitializeComponent();
+        Title = "Padel Club Management";
+        Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/icon.ico"));
+        Background = new SolidColorBrush(Color.FromRgb(90, 214, 255));
 
-        public MainWindow()
-        {
-            InitializeComponent();
-            Title = "Padel Club Management";
-            Icon = new BitmapImage(new Uri("pack://application:,,,/Resources/icon.ico"));
-            Background = new SolidColorBrush(Color.FromRgb(90, 214, 255));
+        SetupDatabase();
+        LoadPlayers();
+    }
 
-            SetupDatabase();
-            LoadPlayers();
-        }
+    private void SetupDatabase()
+    {
+        DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
+        optionsBuilder.UseSqlite(@"Data Source=..\..\..\..\PadelClubManagement.db");
+        _context = new PadelClubManagementDbContext(optionsBuilder.Options);
+        DbContextRepository dbContextRepository = new DbContextRepository(_context);
+        bool databaseCreated = _context.Database.EnsureCreated();
+        if (databaseCreated) DataSeeder.Seed(_context);
+        _manager = new Manager(dbContextRepository);
+    }
 
-        private void SetupDatabase()
-        {
-            DbContextOptionsBuilder optionsBuilder = new DbContextOptionsBuilder();
-            optionsBuilder.UseSqlite(@"Data Source=..\..\..\..\PadelClubManagement.db");
-            _context = new PadelClubManagementDbContext(optionsBuilder.Options);
-            DbContextRepository dbContextRepository = new DbContextRepository(_context);
-            bool databaseCreated = _context.Database.EnsureCreated();
-            if (databaseCreated) DataSeeder.Seed(_context);
-            _manager = new Manager(dbContextRepository);
-        }
-
-        private void LoadPlayers()
-        {
-            playersListBox.Items.Clear(); // Clear the list before adding new items
+    private void LoadPlayers()
+    {
+        playersListBox.Items.Clear(); // Clear the list before adding new items
             
-            IEnumerable<Player> players = _manager.GetAllPlayers();
+        IEnumerable<Player> players = _manager.GetAllPlayers();
 
-            foreach (Player player in players)
-            {
-                playersListBox.Items.Add($"Player Number: {player.PlayerNumber}");
-                playersListBox.Items.Add($"First Name: {player.FirstName}");
-                playersListBox.Items.Add($"Last Name: {player.LastName}");
-                playersListBox.Items.Add($"Birth Date: {player.BirthDate}");
-                playersListBox.Items.Add($"Level: {player.Level}");
-                playersListBox.Items.Add($"Position: {player.Position}");
-                playersListBox.Items.Add("");
-            }
-        }
-
-        private void RefreshButton_Click(object sender, RoutedEventArgs e)
+        foreach (Player player in players)
         {
-            LoadPlayers();
+            playersListBox.Items.Add($"Player Number: {player.PlayerNumber}");
+            playersListBox.Items.Add($"First Name: {player.FirstName}");
+            playersListBox.Items.Add($"Last Name: {player.LastName}");
+            playersListBox.Items.Add($"Birth Date: {player.BirthDate}");
+            playersListBox.Items.Add($"Level: {player.Level}");
+            playersListBox.Items.Add($"Position: {player.Position}");
+            playersListBox.Items.Add("");
         }
+    }
+
+    private void RefreshButton_Click(object sender, RoutedEventArgs e)
+    {
+        LoadPlayers();
     }
 }
