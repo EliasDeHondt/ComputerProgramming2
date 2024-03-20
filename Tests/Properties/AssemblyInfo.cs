3085ea8c62d2bb937a747cd72243072c35edb9e4
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-20 20:29:52 +0100

    Sprint 8 CI

---
+++ Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,10 @@
+/***************************************
+ *                                     *
+ *   Created by Elias De Hondt         *
+ *   Visit https://eliasdh.com         *
+ *                                     *
+ ***************************************/
+
+using Xunit;
+
+[assembly: CollectionBehavior(DisableTestParallelization = true)]
