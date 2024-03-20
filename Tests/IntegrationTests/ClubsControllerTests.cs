0a8babc318be810db70149bea187522776002d92
EliasDeHondt <Elias.dehondt@outlook.com>
2024-03-20 20:59:34 +0100

    Separating test results.

--- Tests/IntegrationTests/ClubsControllerTests.cs
+++ Tests/IntegrationTests/ClubsControllerTests.cs
@@ -13,20 +13,16 @@ using Xunit;
 
 namespace Tests.IntegrationTests;
 
-public class ClubsControllerTests : IClassFixture<ExtendedWebApplicationFactoryWithMockAuth<Program>>
+// This test class is different as the rest. Here we have ensured that the tests are independent of each other by using a new scope for each test.
+// Because in the second test we delete all information in the database. This way we can be sure that the second test is not dependent on the first test.
+public class ClubsControllerTests
 {
-    private readonly ExtendedWebApplicationFactoryWithMockAuth<Program> _factory;
-    
-    public ClubsControllerTests(ExtendedWebApplicationFactoryWithMockAuth<Program> factory) // Constructor
-    {
-        _factory = factory; // Create a new web application factory
-    }
-    
-    [Fact]                                                     // [HttpGet("/api/clubs")]
+    [Fact]                                                      // [HttpGet("/api/clubs")]
     public void GetAllClubs_Return200_GivenValidEndpointData() // Method: public IActionResult GetAllClubs();
     {
         // Arrange
-        var client = _factory.CreateClient(); // Create an HTTP client
+        using var factory = new ExtendedWebApplicationFactoryWithMockAuth<Program>();
+        var client = factory.CreateClient(); // Create an HTTP client
         
         // Act
         var response = client.GetAsync("/api/clubs").Result; // Send a GET request to the specified URI
@@ -34,28 +30,25 @@ public class ClubsControllerTests : IClassFixture<ExtendedWebApplicationFactoryW
         // Assert
         Assert.Equal(HttpStatusCode.OK, response.StatusCode); // Expected: 200
         Assert.NotNull(response.Content); // Expected: Not null
-        Assert.True(response.Content.Headers.ContentLength > 0); // Expected: True
+        Assert.True(response.Content.Headers.ContentLength > 0);  // Expected: Content length > 0
     }
-    
-    [Fact]                                                       // [HttpGet("/api/clubs")]
+
+    [Fact]                                                        // [HttpGet("/api/clubs")]
     public void GetAllClubs_Return204_GivenInValidEndpointData() // Method: public IActionResult GetAllClubs();
     {
         // Arrange
-        DropDatabase(); // Drop the database
-        var client = _factory.CreateClient(); // Create an HTTP client
+        using var factory = new ExtendedWebApplicationFactoryWithMockAuth<Program>();
+        using var scope = factory.Services.CreateScope(); // Create a new scope
+        var services = scope.ServiceProvider; // Get the service provider
+        var manager = services.GetRequiredService<IManager>(); // Get the manager
+        manager.DeleteAllClubs(); // Delete all clubs
+
+        var client = factory.CreateClient(); // Create an HTTP client
         
         // Act
         var response = client.GetAsync("/api/clubs").Result; // Send a GET request to the specified URI
         
         // Assert
-        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode);  // Expected: 204
-    }
-    
-    private void DropDatabase()
-    {
-        using IServiceScope scope = _factory.Services.CreateScope();
-        IServiceProvider services = scope.ServiceProvider;
-        IManager manager = services.GetRequiredService<IManager>();
-        manager.DeleteAllClubs(); // Delete all clubs
+        Assert.Equal(HttpStatusCode.NoContent, response.StatusCode); // Expected: 204
     }
 }
